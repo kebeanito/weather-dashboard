f80847229149bd1979fe4dc649cf553df513f1e5
kebeanito <projectpurposes1220@gmail.com>
2026-01-06 02:58:57 +0800

    Moved API Key to application.properties and updated WeatherService

--- src/main/java/com/example/weatherdashboard/service/WeatherService.java
+++ src/main/java/com/example/weatherdashboard/service/WeatherService.java
@@ -1,18 +1,21 @@
 package com.example.weatherdashboard.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
 
-    private final String API_KEY = "f7994936766874f3fa5bad0417fd2cb0";
+    @Value("${weather.api.key}")
+    private String apiKey;
+
     private final RestTemplate restTemplate = new RestTemplate();
 
     public String getWeatherData(String city) {
         try {
             String url = "https://api.openweathermap.org/data/2.5/weather?q="
-                    + city + "&appid=" + API_KEY + "&units=metric";
+                    + city + "&appid=" + apiKey + "&units=metric";
 
             // automatically performs the GET request
             return restTemplate.getForObject(url, String.class);
